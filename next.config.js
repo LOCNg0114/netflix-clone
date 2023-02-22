7c79a921ddfa4fc5d43b8df75ba4a3cdd59b0799
locnt1 <locnt1@dtp-education.com>
2023-02-22 15:32:09 +0700

    update responsive UI for web page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  // images: {
+  //   domains: ['tmdb.org', 'themoviedb.org'],
+  // },
+  images: { 
+    loader: "default", 
+    minimumCacheTTL: 60, 
+    domains: [ 
+      "image.tmdb.org", 
+      'tmdb.org', 
+      'themoviedb.org'
+    ],
+  },
 }
